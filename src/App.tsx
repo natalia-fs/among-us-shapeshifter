b8378813f314f6bc6bcf6a6b9a83219b86800fc8
Natalia <natalia-321s@outlook.com>
2022-07-06 23:05:20 -0300

    feat: rendering crewmate with HTML and CSS
    Just copying code from another repo I have

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Crewmate } from './components/Crewmate'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Crewmate />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Crewmate/index.tsx
@@ -0,0 +1,17 @@
+import './styles.css';
+
+export function Crewmate(){
+  const color: string = '#4fbfd3';
+  document.documentElement.style.setProperty('--color',color)
+  return (
+    <div className="draw">
+      <div className="crew-shadow" style={{background: color}}></div>
+      <div className="crew">
+        <div className="legs-shadow"></div>
+        <div className="legs"></div>
+        <div className="back"></div>
+        <div className="glass"></div>
+      </div>
+    </div>
+  )
+}
