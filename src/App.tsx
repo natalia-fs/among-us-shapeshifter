d23c6b33d20d7e4b828709497dbb03272d792cb3
Natalia <natalia-321s@outlook.com>
2022-07-16 23:17:49 -0300

    feat: refatorando o contexto + shuffle dos tripulantes
    Embaralhando o grid ao selecionar o metamorfo

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,17 @@
-import { ToastContainer } from 'react-toastify';
 import './App.css'
 import { Crewmate } from './components/Crewmate'
-import { CrewmatesProvider, Crewmate as CrewmateType } from './CrewmatesContext';
+import { Crewmate as CrewmateType, useCrewmates } from './CrewmatesContext';
 
 function App() {
-  const crewmates = [
-    {id: 'Red', color:'#c51111'},
-    {id: 'Blue', color: '#132ed1'},
-    {id: 'Green', color:'#117f2d'},
-    {id: 'Pink', color: '#fb69be'},
-    {id: 'Orange', color: '#e8790e'},
-    {id: 'Yellow', color: '#f5f557'},
-    {id: 'Black', color: '#3e474e'},
-    {id: 'White', color: '#d6e1f3'},
-    {id: 'Purple', color: '#6d3ec0'},
-    {id: 'Brown', color: '#764a1d'},
-    {id: 'Cyan', color: '#28eae1'},
-    {id: 'Lime', color: '#45d633'},
-    {id: 'Maroon', color: '#6b2b3c'},
-    {id: 'Rose', color: '#ecc0d3'},
-    {id: 'Banana', color: '#fffebe'},
-    {id: 'Gray', color: '#8397a7'},
-    {id: 'Tan', color: '#928776'},
-    {id: 'Coral', color: '#ec7578'},
-    {id: 'Dodgerblue', color: '#1e90ff'},
-  ];
 
-  const sliceLimit = crewmates.length;
+  const { crew } = useCrewmates();
 
-  function shuffleCrewmates(crew: Array<CrewmateType>, sliceLimit: number): Array<CrewmateType>{
-    let slicedCrewmateArray = crew.slice(0, sliceLimit);
-    let currentIndex = slicedCrewmateArray.length+1;
-    let randomIndex;
-    // Alterando a string do id, para diferenciar os tripulantes clonados
-    let shapeshifter = slicedCrewmateArray[Math.floor(Math.random() * (currentIndex-1) )];
-    shapeshifter.id+='*';
-    slicedCrewmateArray.push({id: shapeshifter.id.replace('*','#'), color: shapeshifter.color});
-    while(currentIndex !== 0){
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-
-    [slicedCrewmateArray[currentIndex], slicedCrewmateArray[randomIndex]] = [slicedCrewmateArray[randomIndex], slicedCrewmateArray[currentIndex]]; }
-
-    return slicedCrewmateArray;
-  }
   return (
-    <CrewmatesProvider>
-      <ToastContainer
-        position="top-right"
-        autoClose={4000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        limit={4}
-        pauseOnHover
-        pauseOnFocusLoss={false}
-        draggable
-      />
+    <>
       <div className="App">
         <div className="gridContainer">
           {
-            shuffleCrewmates(crewmates, sliceLimit).map((crewmate: CrewmateType) => {
+            crew.map((crewmate: CrewmateType) => {
               return (
                 <Crewmate crewmate={crewmate} key={crewmate.id}/>
               )
@@ -69,7 +19,7 @@ function App() {
           }
         </div>
       </div>
-    </CrewmatesProvider>
+    </>
   )
 }
 
--- src/CrewmatesContext.tsx
+++ src/CrewmatesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, ReactNode } from "react";
+import { createContext, useContext, ReactNode, useState, useEffect } from "react";
 import { toast } from 'react-toastify';
 
 export interface Crewmate{
@@ -11,23 +11,76 @@ interface CrewmateProviderProps {
 }
 
 interface CrewmatesContextData {
+  crew: Crewmate[];
   selectCrewmate: (crewmate: Crewmate) => void;
+  shuffleCrewmates: (crewmates: Crewmate[], sliceLimit: number) => Crewmate[];
 }
 
-const CrewmatesContext = createContext<CrewmatesContextData>({} as CrewmatesContextData);
+const CrewmatesContext = createContext<CrewmatesContextData>(
+  {} as CrewmatesContextData
+);
+
+const crewmates = [
+  {id: 'Red', color:'#c51111'},
+  {id: 'Blue', color: '#132ed1'},
+  {id: 'Green', color:'#117f2d'},
+  {id: 'Pink', color: '#fb69be'},
+  {id: 'Orange', color: '#e8790e'},
+  {id: 'Yellow', color: '#f5f557'},
+  {id: 'Black', color: '#3e474e'},
+  {id: 'White', color: '#d6e1f3'},
+  {id: 'Purple', color: '#6d3ec0'},
+  {id: 'Brown', color: '#764a1d'},
+  {id: 'Cyan', color: '#28eae1'},
+  {id: 'Lime', color: '#45d633'},
+  {id: 'Maroon', color: '#6b2b3c'},
+  {id: 'Rose', color: '#ecc0d3'},
+  {id: 'Banana', color: '#fffebe'},
+  {id: 'Gray', color: '#8397a7'},
+  {id: 'Tan', color: '#928776'},
+  {id: 'Coral', color: '#ec7578'},
+  {id: 'Dodgerblue', color: '#1e90ff'},
+];
+
+const sliceLimit = crewmates.length;
 
 export function CrewmatesProvider( { children }: CrewmateProviderProps ){
   // const [crewmates, setCrewmates] = useState<Crewmate[]>([]);
-  // Usar caso um dia eu pegue os dados via api
-  // useEffect(() => {
-  //   api.get('crewmates')
-  //     .then(response => setCrewmates(response.data.crewmates))
-  // }, []);
+  const [ crew, setCrew ] = useState<Crewmate[]>( () => {
+    return shuffleCrewmates(crewmates, sliceLimit);
+  })
+
+  function shuffle(crewArray: Array<Crewmate>, sliceLimit: number): Crewmate[]{
+    let slicedCrewmateArray = crewArray.slice(0, sliceLimit);
+    let currentIndex = slicedCrewmateArray.length+1;
+    let randomIndex;
+    // Alterando a string do id, para diferenciar os tripulantes clonados
+    let shapeshifter = slicedCrewmateArray[Math.floor(Math.random() * (currentIndex-1) )];
+    shapeshifter.id+='*';
+    slicedCrewmateArray.push({
+      id: shapeshifter.id.replace('*','#'),
+      color: shapeshifter.color
+    });
+    while(currentIndex !== 0){
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+     [slicedCrewmateArray[currentIndex], slicedCrewmateArray[randomIndex]] = [slicedCrewmateArray[randomIndex], slicedCrewmateArray[currentIndex]];
+    }
+    return slicedCrewmateArray;
+  }
+
+  function shuffleCrewmates(crewArray: Array<Crewmate>, sliceLimit: number): Crewmate[]{
+    return shuffle(crewArray, sliceLimit);
+  }
 
   function selectCrewmate(crewmate: Crewmate){
     if( crewmate.id.includes('*') ){
       toast.success(`Parabéns, você encontrou o metamorfo!   ${crewmate.id} diz: "Não sou eu! CONFIA"`, {
-        position: toast.POSITION.BOTTOM_CENTER
+        position: toast.POSITION.BOTTOM_CENTER,
+        onClose: () => {
+          setCrew(shuffleCrewmates(crewmates, sliceLimit));
+        }
       });
     }
     else if( crewmate.id.includes('#') )
@@ -42,7 +95,7 @@ export function CrewmatesProvider( { children }: CrewmateProviderProps ){
   }
 
   return (
-    <CrewmatesContext.Provider value={{ selectCrewmate }}>
+    <CrewmatesContext.Provider value={{ crew, selectCrewmate, shuffleCrewmates }}>
       { children }
     </CrewmatesContext.Provider>
   )
