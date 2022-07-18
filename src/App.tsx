2b3c794c1cb020a1f0387ee48fc59e57bda2803a
Natalia <natalia-321s@outlook.com>
2022-07-17 23:07:45 -0300

    feat: modals (start and game over) + score and timer
    feat: Refactoring CrewmateContext
    lib: @types/node

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import './App.css'
 import { Crewmate } from './components/Crewmate'
+import { Timer } from './components/Timer';
 import { Crewmate as CrewmateType, useCrewmates } from './CrewmatesContext';
+import './App.css'
 
 function App() {
 
-  const { crew } = useCrewmates();
+  const { crew, score } = useCrewmates();
 
   return (
     <>
       <div className="App">
+        <div className="infosContainer">
+          <div className="score"> Pontuação:
+            <span>{score}</span>
+          </div>
+          <Timer />
+
+        </div>
+
         <div className="gridContainer">
           {
             crew.map((crewmate: CrewmateType) => {
